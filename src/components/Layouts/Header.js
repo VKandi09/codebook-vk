bfc96a3fa1145eb025bcba2d6edc006965368091
Varsha Kandi <kandivarsha09@gmail.com>
2024-12-01 20:44:08 -0500

    Updated website icon and title

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -26,8 +26,8 @@ export const Header = () => {
         <nav className="bg-white dark:bg-gray-900">
             <div className="border-b border-slate-200 dark:border-b-0 flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-3">
                 <Link to="/" className="flex items-center">
-                    <img src={Logo} className="mr-3 h-10" alt="CodeBook Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CodeBook</span>
+                    <img src={Logo} className="mr-3 h-10" alt="eBook Hub Logo" />
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">eBook Hub</span>
                 </Link>
                 <div className="flex items-center relative">
                     <span onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-gear-wide-connected"></span>
